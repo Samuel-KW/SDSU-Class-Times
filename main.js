70145c27774e13d5cef258e45bd904c1b1a09e02
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2023-08-18 17:44:41 -0700

    started canvas work

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const c = document.createElement('canvas');
-const ctx = c.getContext('2d');
+class CanvasHandler {
+    constructor () {
+        this.c = document.createElement('canvas');
+        this.ctx = c.getContext('2d');
 
-document.body.appendChild(c);
+        this.width = c.width;
+        this.height = c.height;
+    }
 
 
+    init () {
+
+        c.addEventListener('resize', () => this.resize());
+        this.draw();
+
+        document.body.appendChild(c);
+    }
+
+    resize () {
+        this.width = c.width;
+        this.height = c.height;
+    }
+
+    draw () {
+
+    }
+}
+
+const Session = new CanvasHandler();
